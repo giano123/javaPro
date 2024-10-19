27ffb55fae76f9c2e64d52fbf4634e7faf1943cb
giano123 <gianoswald2@gmail.com>
2024-10-19 16:38:30 +0200

    Java project

---
+++ javaProject.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class javaProject {
+    public static void main(String[] args) {
+        int alter = 17;
+
+        if(alter >= 16){
+            System.out.println("Du darfst Motorrad Fahren");
+        } else {
+            System.out.println("Du darfst kein Motorrad Fahren");
+        }
+    }
+}
